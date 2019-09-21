7d04611171a4c51cc34c0df4199ded1e1c0464d9
SzedlacsekGigi <szedlacsek.lilla@gmail.com>
2019-09-21 09:17:36 +0200

    updated videorecommendations application properties

---
+++ videorecommendation/src/main/java/com/codecool/videorecommendation/VideorecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.videorecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideorecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideorecommendationApplication.class, args);
+	}
+
+}
